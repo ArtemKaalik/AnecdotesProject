d26357e3fb9a126d6d668067ce1c6ada91f89654
Artem Kaalik <artjom.kaalik@ivkhk.ee>
2023-06-15 17:46:55 +0300

    Style and Code update

--- register.js
+++ register.js
@@ -1,22 +1,19 @@
-window.onload = function() {
-  console.log('testt');
-  var registerForm = document.getElementById("registerForm");
+document.getElementById("registerForm");
 
-  registerForm.addEventListener("submit", function(event) {
-    console.log("test");
-    event.preventDefault(); // Предотвратить отправку формы по умолчанию
+registerForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Предотвратить отправку формы по умолчанию
 
-    var name = registerForm.elements.name.value;
-    var login = registerForm.elements.login.value;
-    var password = registerForm.elements.password.value;
+  var name = registerForm.elements.name.value;
+  var login = registerForm.elements.login.value;
+  var password = registerForm.elements.password.value;
 
-    // Отправка данных на сервер
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/register", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-      console.log('error');
-      if (xhr.readyState === 4 && xhr.status === 200) {
+  // Отправка данных на сервер
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/register", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
         var response = JSON.parse(xhr.responseText);
         // Обработка ответа от сервера
         if (response.success) {
@@ -25,9 +22,11 @@ window.onload = function() {
         } else {
           alert("Ошибка регистрации: " + response.message);
         }
+      } else {
+        alert("Произошла ошибка при выполнении запроса. Пожалуйста, попробуйте снова.");
       }
-    };
-    var data = JSON.stringify({ name: name, login: login, password: password });
-    xhr.send(data);
-  });
-};
+    }
+  };
+  var data = JSON.stringify({ name: name, login: login, password: password });
+  xhr.send(data);
+});
